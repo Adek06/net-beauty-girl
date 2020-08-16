6dd60da73c4f0ddae10138aebd4bae514965b1f9
adek06 <adek06@outlook.com>
2020-08-16 14:27:54 +0000

    feat(config): add heroku config

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,15 @@
 class ApiController < ActionController::API
-    BOT_TOKEN = ""
-    TWITTER_HEADER = {"Cookie": '', "x-csrf-token": '', "authorization": ''}
+    if ENV['BOT_TOKEN']
+      BOT_TOKEN = ENV['BOT_TOKEN']
+    else
+      BOT_TOKEN = ""
+    end
+    
+    if ENV['Cookie']
+      TWITTER_HEADER = {"Cookie": ENV['Cookie'], "x-csrf-token":ENV['x-csrf-token'], "authorization": ENV['authorization']}
+    else
+    
+      TWITTER_HEADER = {"Cookie": '', "x-csrf-token": '', "authorization": ''}
+
+    end
 end
