e1d184bad94907d3576047a57f539b383213df7f
YouST <adek06@163.com>
2020-08-23 21:43:31 +0800

    feat(ins): add ins downloader

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -11,8 +11,57 @@ class WebhookController < ApiController
     if text.match(/^(https|http):\/\/twitter/)
       download_from_twitter @@TWITTER_HEADER, params
       return
+    elsif text.match(/^(https|http):\/\/www.instagram/)
+      download_from_ins params
+      return
     end
   end
+end
+
+def download_from_ins(params)
+  text = params['message']['text']
+  chat = params['message']['chat']
+  chat_id = chat['id']
+  if chat['username'] != ENV['USERNAME']
+    return '404'
+  end
+  url = text.split('?')[0] + '?utm_source=ig_web_button_share_sheet'
+  res = HTTP.get(url).to_s
+  m = res.match(/window._sharedData = (.{1,99999});\<\/script\>/)
+  j = JSON.parse(m[1])
+
+  img_list = []
+  if j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']
+    nodes = j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
+
+    nodes.each do |node|
+      img_list << node['node']['display_resources'][-1]['src']
+    end
+  else
+    m = res.match(/meta property="og:image" content="(.{1,99999})" \/>/)
+    img_list << m[1]
+  end
+
+  caption = ''
+  if j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_caption']['edges'][0]
+    caption = j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_caption']['edges'][0]['node']['text']
+  end
+  record = GirlPhotoPost.create(url: url, imgs: img_list, content: caption, isPush: false)
+  media = Array.new()
+  img_list.each do |img|
+    temp = {}
+    temp['type'] = 'photo'
+    temp['media'] = img
+    media << temp
+  end
+  media[0]['caption'] = caption
+  tele_json = {}
+  tele_json['media'] = media
+  tele_json['chat_id'] = chat_id
+  bot = TeleBot.new ENV['BOT_TOKEN']
+  bot.send_photos tele_json
+  p "send to #{tele_json['chat_id']}"
+  render json: record
 end
 
 def download_from_twitter(twitter_header, params)
